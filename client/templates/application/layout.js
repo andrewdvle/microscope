69483a64cebf9fc513f0137333750fb6d0693e55
andrewle <andrewle@amazon.com>
2015-06-10 03:40:18 -0700

    Fade during page transitions.
    Fade in and fade out items.

---
+++ client/templates/application/layout.js
@@ -0,0 +1,17 @@
+// Page fade, exactly same as the one we used for post insertion/removal
+
+Template.layout.onRendered(function() {
+  this.find('#main')._uihooks = {
+    insertElement: function(node, next) {
+      $(node)
+        .hide()
+        .insertBefore(next)
+        .fadeIn();
+    },
+    removeElement: function(node) {
+      $(node).fadeOut(function() {
+        $(this).remove();
+      });
+    }
+  }
+});
--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -6,6 +6,15 @@ Template.postsList.helpers({
 
 Template.postsList.onRendered(function () {
   this.find('.wrapper')._uihooks = {
+
+   // fade in new posts
+    insertElement: function (node, next) {
+      $(node)
+        .hide()
+        .insertBefore(next)
+        .fadeIn();
+     },
+
     moveElement: function (node, next) {
       var $node = $(node), $next = $(next);
       var oldTop = $node.offset().top;
@@ -34,11 +43,16 @@ Template.postsList.onRendered(function () {
       // force a redraw
       $node.offset();
 
-
-
       // reset everything to 0, animated
       $node.addClass('animate').css('top', 0);
       $inBetween.addClass('animate').css('top', 0);
+    },
+    
+    // fade out deleted elements
+      removeElement: function(node) {
+      $(node).fadeOut(function() {
+        $(this).remove();
+      });
     }
   }
 });
