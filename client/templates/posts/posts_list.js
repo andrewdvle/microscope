359607ed7c1098479bc73811a09f60221c5bf698
andrewle <andrewle@amazon.com>
2015-06-10 03:31:52 -0700

    Added post reordering animation.

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -3,3 +3,42 @@ Template.postsList.helpers({
       return Posts.find({}, {sort: {submitted: -1}});;
    }
 });
+
+Template.postsList.onRendered(function () {
+  this.find('.wrapper')._uihooks = {
+    moveElement: function (node, next) {
+      var $node = $(node), $next = $(next);
+      var oldTop = $node.offset().top;
+      var height = $node.outerHeight(true);
+
+      // find all the elements
+      var $inBetween = $next.nextUntil(node);
+      if ($inBetween.length === 0)
+        $inBetween = $node.nextUntil(next);
+
+      $node.insertBefore(next);
+
+      // get the new top 
+      var newTop = $node.offset().top;
+
+      // move node to its original spot
+      $node
+        .removeClass('animate')
+        .css('top', oldTop - newTop);
+
+      // push every other element down (or up) to put them back
+      $inBetween
+        .removeClass('animate')
+        .css('top', oldTop < newTop ? height : -1 * height)
+
+      // force a redraw
+      $node.offset();
+
+
+
+      // reset everything to 0, animated
+      $node.addClass('animate').css('top', 0);
+      $inBetween.addClass('animate').css('top', 0);
+    }
+  }
+});
